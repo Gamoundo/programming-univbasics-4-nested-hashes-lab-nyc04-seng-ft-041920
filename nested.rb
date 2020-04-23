f48fcc178bf3e65a4d94399d7024ceda86f5c67f
Akil Griffiths <akilgg@gmail.com>
2020-04-23 16:18:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
       }
     }
 
+programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -82,6 +84,11 @@ def adding_matz
       }
     }
 
+programmer_hash[:yukihiro_matsumoto] = {
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"]
+}
+
 end
 
 def changing_alan
@@ -104,6 +111,9 @@ def changing_alan
       }
     }
 
+programmer_hash[:alan_kay][:known_for] = alans_new_info
+programmer_hash
+
 end
 
 def adding_to_dennis
@@ -125,5 +135,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 
 end
